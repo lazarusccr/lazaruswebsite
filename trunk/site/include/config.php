3e27d3f1cfc975041f1a7b83ab15aafeeb6841e8
juha <juha@07e2e39b-4867-4bc1-a305-3bbf0e64bf7e>
2013-06-22 16:29:21 +0000

    Update new Lazarus version number to all places.

--- trunk/site/include/config.php
+++ trunk/site/include/config.php
@@ -40,15 +40,15 @@ $dl_latest_version_fpc = '2.6.2';
 // http://www.miscdebris.net/blog/2010/04/06/use-curl-to-download-a-file-from-sourceforge-mirror/
 // http://www.google.com/url?q=http%3A%2F%2Fwww.php.net%2Fmanual%2Fen%2Ffunction.curl-getinfo.php&sa=D&sntz=1&usg=AFQjCNEtR7MVQB4n7Q1b3u5iwZOFQgHyYQ
 
-$dl_win32_url	= 'http://sourceforge.net/projects/lazarus/files/Lazarus%20Windows%2032%20bits/Lazarus%201.0.8/lazarus-1.0.8-fpc-2.6.2-win32.exe/download';
-$dl_win64_url	= 'http://sourceforge.net/projects/lazarus/files/Lazarus%20Windows%2064%20bits/Lazarus%201.0.8/lazarus-1.0.8-fpc-2.6.2-win64.exe/download';
-$dl_deb32_url	= 'http://sourceforge.net/projects/lazarus/files/Lazarus%20Linux%20i386%20DEB/Lazarus%201.0.8/';
-$dl_deb64_url	= 'http://sourceforge.net/projects/lazarus/files/Lazarus%20Linux%20amd64%20DEB/Lazarus%201.0.8/';
-$dl_rpm32_url	= 'http://sourceforge.net/projects/lazarus/files/Lazarus%20Linux%20i386%20RPM/Lazarus%201.0.8/';
-$dl_rpm64_url	= 'http://sourceforge.net/projects/lazarus/files/Lazarus%20Linux%20x86_64%20RPM/Lazarus%201.0.8/';
-$dl_mac32_url	= 'http://sourceforge.net/projects/lazarus/files/Lazarus%20Mac%20OS%20X%20i386/Lazarus%201.0.8/';
-$dl_macppc_url	= 'http://sourceforge.net/projects/lazarus/files/Lazarus%20Mac%20OS%20X%20powerpc/Lazarus%201.0.8/';
-$dl_sources_url	= 'http://sourceforge.net/projects/lazarus/files/Lazarus%20Zip%20_%20GZip/Lazarus%201.0.8/';
+$dl_win32_url	= 'http://sourceforge.net/projects/lazarus/files/Lazarus%20Windows%2032%20bits/Lazarus%201.0.10/lazarus-1.0.10-fpc-2.6.2-win32.exe/download';
+$dl_win64_url	= 'http://sourceforge.net/projects/lazarus/files/Lazarus%20Windows%2064%20bits/Lazarus%201.0.10/lazarus-1.0.10-fpc-2.6.2-win64.exe/download';
+$dl_deb32_url	= 'http://sourceforge.net/projects/lazarus/files/Lazarus%20Linux%20i386%20DEB/Lazarus%201.0.10/';
+$dl_deb64_url	= 'http://sourceforge.net/projects/lazarus/files/Lazarus%20Linux%20amd64%20DEB/Lazarus%201.0.10/';
+$dl_rpm32_url	= 'http://sourceforge.net/projects/lazarus/files/Lazarus%20Linux%20i386%20RPM/Lazarus%201.0.10/';
+$dl_rpm64_url	= 'http://sourceforge.net/projects/lazarus/files/Lazarus%20Linux%20x86_64%20RPM/Lazarus%201.0.10/';
+$dl_mac32_url	= 'http://sourceforge.net/projects/lazarus/files/Lazarus%20Mac%20OS%20X%20i386/Lazarus%201.0.10/';
+$dl_macppc_url	= 'http://sourceforge.net/projects/lazarus/files/Lazarus%20Mac%20OS%20X%20powerpc/Lazarus%201.0.10/';
+$dl_sources_url = 'http://sourceforge.net/projects/lazarus/files/Lazarus%20Zip%20_%20GZip/Lazarus%201.0.10/';
 
 
 //---- SMF Board Number / Index ----//
@@ -78,4 +78,4 @@ $website_domain = 'http://' . $_SERVER['HTTP_HOST'];
 // --- Pagination --- //
 //$items_per_page = 10; // in case of pagination
 
-?>
+?>
