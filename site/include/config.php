8862bd3aac832009e3c1d0e7751cdd68c718b265
martin <martin@07e2e39b-4867-4bc1-a305-3bbf0e64bf7e>
2015-04-19 01:23:31 +0000

    update to 1.4

--- site/include/config.php
+++ site/include/config.php
@@ -30,8 +30,8 @@ $home_php_name = 'index.php';
 
 //------ Download Information (Link/version/mirror...) -----//
 //--- versions ---
-$dl_latest_version_lazarus = '1.2.6';
-$dl_latest_version_dir= '1.2.6';  // could be 1.2, for version 1.2.0
+$dl_latest_version_lazarus = '1.4.0';
+$dl_latest_version_dir= '1.4';  // could be 1.2, for version 1.2.0
 // the FPC version that is required by the latest lazarus
 $dl_latest_version_fpc = '2.6.4';
 
