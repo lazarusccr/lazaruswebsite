32a5449d3f1cf6ea410a60a620311d822e89e586
martin <martin@07e2e39b-4867-4bc1-a305-3bbf0e64bf7e>
2020-04-28 01:26:27 +0000

    Update copyright to 2020

--- site/include/config.php
+++ site/include/config.php
@@ -28,7 +28,7 @@ $error_webmaster_email = 'lazarus@freepascal.org';
 $home_php_name = 'index.php';
 
 
-$copyrightyear = '2019';
+$copyrightyear = '2020';
 
 //------ Download Information (Link/version/mirror...) -----//
 //--- versions ---
